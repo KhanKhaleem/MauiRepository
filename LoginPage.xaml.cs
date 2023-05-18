edc8f9ec8389949ecd46405c4c5334bc107b9218
Kaleemkhan <kaleem.khan@dealmoney.in>
2023-05-18 18:08:03 +0530

    Login

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,15 +1,28 @@
+using MyFirstMAUI_Project.Models;
+using MyFirstMAUI_Project.Servises;
 using MyFirstMAUI_Project.ViwModels;
 
 namespace MyFirstMAUI_Project;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
+//     ILoginRepository repository;
+//    public LoginPage(ILoginRepository repository)
+//      {
+//         this.repository = repository;
+//      }
+    public LoginPage()
 	{
 		InitializeComponent();
     }
     public async void OnSignUpTapped(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SignInPage());
+    }
+    public void SingInClickd(object sender, EventArgs e)
+    {
+        //string username = UserName.Text;
+        //string password = Password.Text;
+        //repository.Login(username, password);
     }
 }
